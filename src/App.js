1f68e6e6c635d236165b88a427fe5aaae8fdbabe
EricChukoltyansky <kazohelmet@gmail.com>
2022-11-19 16:26:51 +0200

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,78 +1,97 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
+import { useEffect, useReducer, useState } from "react";
+// useReducer why we do it
 
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "add_artist":
+//       return { ...state, artist: [...state.artist, action.newArtist] };
+//     case "delete_artist":
+//       return {
+//         ...state,
+//         artist: state.artist.filter((artist) => artist.id !== action.id),
+//       };
+//     case "add_like":
+//       return {
+//         ...state,
+//         artist: state.artist.map((artist) => {
+//           if (artist.id === action.id) {
+//             return { ...artist, like: artist.like + 1 };
+//           }
+//           return artist;
+//         }),
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const initialState = {
+//   artist: [
+//     { name: "Bob", song: "One Love", id: 1, like: 0 },
+//     { name: "Micheal", song: "Black or white", id: 2, like: 10 },
+//     { name: "Roger", song: "We don't need no education", id: 3, like: 2 },
+//   ],
+// };
 const App = () => {
-  const [joke, setJoke] = useState("");
-  const [categories, setCategories] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
-
-  const getRandomJoke = async () => {
-    const response = await axios.get("https://api.chucknorris.io/jokes/random");
-    console.log(response);
-    setJoke(response.data.value);
+  // const [stateReducer, dispatch] = useReducer(reducer, initialState);
+  const [state, setArtist] = useState([
+    { name: "Bob", song: "One Love", id: 1, like: 0 },
+    { name: "Micheal", song: "Black or white", id: 2, like: 10 },
+    { name: "Roger", song: "We don't need no education", id: 3, like: 2 },
+  ]);
+  const addArtist = (newArtist) => {
+    setArtist([...state, newArtist]);
+    // dispatch({ type: "add_artist", newArtist: newArtist });
   };
-
-  const getCategories = async () => {
-    const response = await axios.get(
-      "https://api.chucknorris.io/jokes/categories"
+  const deleteArtist = (id) => {
+    setArtist(state.filter((artist) => artist.id !== id));
+    // dispatch({ type: "delete_artist", id: id });
+  };
+  const addLikeToArtist = (id) => {
+    setArtist(
+      state.map((artist) => {
+        if (artist.id === id) {
+          return { ...artist, like: artist.like + 1 };
+        }
+        return artist;
+      })
     );
-    console.log(response.data);
-    setCategories(response.data);
+    // dispatch({ type: "add_like", id: id });
   };
-
-  const getJokeFromCategories = (category) => {
-    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setJoke(data.value);
-      });
-  };
-
-  useEffect(() => {
-    // const onSearchSubmit = () => {
-    //   if (searchInput) {
-    //     const searchQuery = searchInput;
-    //     fetch(`https://api.chucknorris.io/jokes/search?query=${searchQuery}`)
-    //       .then((response) => response.json())
-    //       .then((data) => {
-    //         console.log(data.result[0].value);
-    //         setJoke(data.result[0].value);
-    //       });
-    //   }
-    // };
-    // onSearchSubmit();
-
-    const debounce = setTimeout(() => {
-      fetch(`https://api.chucknorris.io/jokes/search?query=${searchInput}`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.result[0].value);
-          setJoke(data.result[0].value);
-        });
-    }, 2000);
-
-    return () => {
-      clearTimeout(debounce);
-    };
-  }, [searchInput]);
 
   return (
     <div>
-      <h1>Chuck Norris Jokes</h1>
-      <button onClick={getRandomJoke}>Get Joke</button>
-      <button onClick={getCategories}>Get categories</button>
-      <h1>{joke}</h1>
-      <div>
-        {categories.map((category) => (
-          <div onClick={(e) => getJokeFromCategories(e.target.textContent)}>
-            {category}
-          </div>
-        ))}
-      </div>
-      <input onChange={(e) => setSearchInput(e.target.value)} />
+      <button
+        onClick={() =>
+          addArtist({
+            name: "Roger",
+            song: "We don't need no education",
+            id: 3,
+            like: 2,
+          })
+        }
+      >
+        Add Artist
+      </button>
+      <button onClick={() => deleteArtist(1)}>Delete Artist</button>
+      <button onClick={() => addLikeToArtist(1)}>Add Like</button>
+
+      {/* {stateReducer.artist.map((artist) => (
+        <div key={artist.id}>
+          <h1>{artist.name}</h1>
+          <h2>{artist.song}</h2>
+          <h3>{artist.like}</h3>
+        </div>
+      ))} */}
+      {state.map((artist) => (
+        <div key={artist.id}>
+          <h1>{artist.name}</h1>
+          <h2>{artist.song}</h2>
+          <h3>{artist.like}</h3>
+        </div>
+      ))}
     </div>
   );
 };
-
 export default App;
